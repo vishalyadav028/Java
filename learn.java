d2c387e64685bfd4b557cbca614a56bfed8b522a
Vishal Yadav <vishal70563@gmail.com>
2026-08-13 16:47:53 +0530

    first java code

---
+++ learn.java
@@ -0,0 +1,207 @@
+import java.util.*;
+public class learn{
+    public static void main(String[] args) {
+            //Question.1
+
+    //  for(int i=1; i<=5; i=i+1){
+    //     System.out.println(i);
+    //     }
+
+    //while
+    // int i=1;
+    // while (i<=5) {
+    //     System.out.println(i);
+    //     i++;
+    // }
+
+    // int i=1;
+    // do{
+    //     System.out.println(i);
+    //     i++;
+    // }while(i<=5);
+
+        //Question.2
+        // for(int i=1; i<=10; i=i+1){
+        //     System.out.println(i);
+        // }
+
+        // int i=1;
+        // while(i<=10){
+        //     System.out.println(i);
+        //     i=i+1;
+        // }
+
+        // int i=1;
+        // do{
+        //     System.out.println(i);
+        //     i++;
+        // }while(i<=10);
+
+        //Question.3
+        // for(int i=51; i<=69; i=i+1){
+        //     System.out.print(" "+i);
+        // }
+
+        // int i=51;
+        // while(i<=69){
+        //     System.out.println(i);
+        //     i=i+1;
+        // }
+
+        // int i=51;
+        // do{
+        //     System.out.println(i);
+        //     i=i+1;
+        // }while(i<=69);
+
+        //Question.4
+        // for(int i=51; i<70; i++){
+        //     System.out.print(" "+i);
+        // }
+        // int i=51;
+        // while(i<70){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // int i=51;
+        // do{
+        //     System.out.println(i);
+        //     i++;
+        // }while(i<70);
+
+        //Question.5
+        // for(int i=1; i<=10; i++){
+        //     if(i==4){
+        //         break;
+        //     }
+        //     System.out.println(i);
+        // }
+        // int i=1;
+        // while(i<=10){
+        //     if (i==5) {
+        //         break;
+        //     }
+        //     System.out.println(i);
+        //     i++;
+        // }
+        // int i=1;
+        // do{
+        //     System.out.println(i);
+        //     i++;
+        // }while(i<=10);
+        
+
+        //Question.6
+        // for(int i=1; i<=10; i=i+1){
+        //     if(i==5){
+        //         continue;
+        //     }
+        //     System.out.print(" "+i);
+        // }
+       
+
+
+        //Question.7 (H.W)
+        // for(int i=1; i<=100; i++){
+        //     System.out.print(" "+i);
+        // }
+        // int i=1;
+        // while(i<=100){
+        //     System.out.print(" "+i);
+        //     i++;
+        // }
+        // int i=1;
+        // do{
+        //     System.out.print(" "+i);
+        //     i++;
+        // }while(i<=100);
+
+        //Question.8 (H.W)
+        // for(int i=100; i>=10-1; i--){
+        //     System.out.println(i);
+        // }
+        // int i=100;
+        // while(i>=1) {
+        //     System.out.print(" "+i);
+        //     i--;
+        // }
+        // int i=100;
+        // do{
+        //     System.out.print(" "+i);
+        //     i--;
+        // }while(i>=1);
+
+        
+                //Question.10 (H.W)
+        // for(int i=1; i<=50; i++){
+        //     System.out.print("\nvishal "+ i);
+        // }
+        // int i=1;
+        // while(i<=50){
+        //     System.out.println(" vishal "+i);
+        //     i++;
+        // }
+        // int i=1;
+        // do{
+        //     System.out.println("vishal "+i);
+        //     i++;
+        // }while(i<=50);
+
+
+        //Question.11 (H.W)
+        // for(int i=50; i>=1; i--){
+        //     System.out.println(i);
+        // }
+        // int i=50;
+        // while(i>=1){
+        //     System.out.println(i);
+        //     i--;
+        // }
+        // int i=50;
+        // do{
+        //     System.out.println(i);
+        //     i--;
+        // }while(i>=1);
+
+            //Question.12 (H.W)
+        // for(char i='A'; i<='Z'; i++){
+        //     System.out.println(i);
+        // }
+        // char i='A';
+        // while(i<='Z'){
+        //     System.out.print(" "+i);
+        //     i++;
+        // }
+    //    char i='A';
+    //     do{
+    //     System.out.println(i);
+    //     i++;
+    //     }while(i<='Z');
+
+
+            //Question.13 (H.W)
+        // for(char i='a'; i<='z'; i++){
+        //     System.out.print(i);
+        // }
+        // char i='a';
+        // while(i<='z'){
+        //     System.out.print(" "+i);
+        //     i++;
+        // }
+        // char i='a';
+        // do{
+        //     System.out.print(" "+i);
+        //     i++;
+        // }while(i<='z');
+
+        System.out.print("Enter the table:");
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        for(int i=1; i<11; i++) {
+        System.out.println(i*n);
+        }
+
+    }
+}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,5 @@
+char i='A';
+        // while(i<='Z'){
+        //     System.out.print(" "+i);
+        //     i++;
+        // }
